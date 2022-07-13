ebc7f37ae58b0589e94f073d5ba82ca4b0e0e979
Felipe Steffan <felip.developer@hotmail.com>
2022-07-12 23:07:23 -0300

    classe, Objeto e atributo

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -20,15 +20,14 @@ public class Main {
                 x.b = sc.nextDouble();
                 x.c = sc.nextDouble();
                 System.out.println("Enter the measures of triangle Y: ");
-
                 y.a = sc.nextDouble();
                 y.b = sc.nextDouble();
                 y.c = sc.nextDouble();
 
-                double p = (x.a + x.b + x.c) / 2.0;
-                double areaX = Math.sqrt(p * (p - x.a) * (p - x.b) * (p - x.c));
-                p = (y.a + y.b + y.c) / 2.0;
-                double areaY = Math.sqrt(p * (p - y.a) * (p - y.b) * (p - y.c));
+
+                double areaX = x.area();
+                double areaY = y.area();
+
                 System.out.printf("entities.Triangle X area: %.4f%n", areaX);
                 System.out.printf("entities.Triangle Y area: %.4f%n", areaY);
                 if (areaX > areaY) {
@@ -37,6 +36,8 @@ public class Main {
                 else {
                     System.out.println("Larger area: Y");
                 }
+
+
                 sc.close();
 
 
